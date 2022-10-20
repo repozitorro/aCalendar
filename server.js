ce0e407b5a54f68463f8f84d1fc908f42ed3fbb8
Bohdan Nedashkivskyi <repozitorro@gmail.com>
2022-10-20 18:48:05 +0300

    configuration for heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/angular-calendar'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/angular-calendar/index.html'));
+});
+app.listen(process.env.PORT || 8080, () => console.log("running..."));
+
